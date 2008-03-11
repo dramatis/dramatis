b214a420b5c3f4fb3ecc334d439aeeea308d9a03
Steven Parkes <smparkes@smparkes.net>
2008-03-10 17:00:53 -0700

    parallel update?

--- spec/dramatis/simple_spec.rb
+++ spec/dramatis/simple_spec.rb
@@ -7,11 +7,16 @@ describe Dramatis::Actor do
   it "should be creatable w/o requiring name" do
 
     f = Class.new do
-      Actor::acts_as self
+      Dramatis::Actor::acts_as self
     end
 
     name = f.new
-    success
+
+    # if we get here, we're fine
+    # is there an rspec "something" for this?
+    # note, this doesn't "test" in autotest,
+    # probably because it's eval'ing other tests that
+    # do bring in Actor::Name
 
   end
 
