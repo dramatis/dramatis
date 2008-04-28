11ce57fb87638e7743117fdab5cf4feb0d239e31
Steven Parkes <smparkes@smparkes.net>
2008-04-28 16:06:30 -0700

    add the recursive version of pingpong

---
+++ examples/pingpong/actor_rec.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+$:.push File.join( File.dirname(__FILE__), "..", "..", "lib" )
+
+require 'dramatis/actor'
+
+class PingPong
+
+  include Dramatis::Actor
+
+  def initialize name
+    actor.enable_call_threading
+    @name = name
+  end
+
+  def pingpong count, partner
+    if count == 0
+      puts "#{@name}: done"
+    else
+      if count % 500 == 0 or count % 500 == 1
+        puts "#{@name}: pingpong #{count}"
+      end
+      partner.pingpong count-1, self
+    end
+    # sleep 0.001
+  end
+
+end
+
+ping = PingPong.new "ping"
+pong = PingPong.new "pong"
+
+ping.pingpong ARGV[0].to_i, pong
+
