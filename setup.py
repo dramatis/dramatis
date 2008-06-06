68bee97be07b58576490595613d87d48fc7a9e84
Steven Parkes <smparkes@smparkes.net>
2008-06-05 20:02:09 -0700

    a draft of the 0.1.0 release; need to fix the docs and the tutorials

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
 from distutils.core import setup
 setup( name='dramatis',
-       version='0.0.1',
-       py_modules=['dramatis'] )
+       version='0.1.0',
+       author='Steven Parkes',
+       author_email='smparkes@smparkes.net',
+       url='http://dramatis.mischance.net',
+       description="an actor library for ruby and python",
+       package_dir = {'':'lib'},
+       packages=[
+               'dramatis',
+               'dramatis.error',
+               'dramatis.future_value',
+               'dramatis.actor',
+               'dramatis.actor.name',
+               'dramatis.runtime',
+               'dramatis.runtime.actor',
+               'dramatis.runtime.continuation',
+               ],
+       )
