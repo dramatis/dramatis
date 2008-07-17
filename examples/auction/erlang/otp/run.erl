2a7fd4853e4b5dd0801c36a97cdff2e736faef85
Steven Parkes <smparkes@smparkes.net>
2008-07-17 11:43:47 -0700

    a tweak for auction otp, though it's still not supported

--- examples/auction/erlang/otp/run.erl
+++ examples/auction/erlang/otp/run.erl
@@ -2,21 +2,13 @@
 -export([run/0]).
 
 run() ->
-    Seller = seller:new(),
-    {_,Seconds,_} = now(),
-    Auction = auction:new( Seller, 100, Seconds + 4 ),
-    C1 = client:new( "1a", 20, 200, Auction ),
-    io:format( "? ~p~n", [ C1 ] ).
-    io:format( "? ~p~n", [ client:namey(C1) ] ).
-
-foo() ->
     % somebody gives up
     Seller = seller:new(),
     {_,Seconds,_} = now(),
     Auction = auction:new( Seller, 100, Seconds + 4 ),
     C1 = client:new( "1a", 20, 200, Auction ),
     client:new( "1a", 10, 300, Auction ),
-    io:format( "? ~p~n", [ client:namey(C1) ] ),
+    io:format( "? ~p~n", [ client:name(C1) ] ),
     io:format( "Notice: client ~p won the first auction with a bid of ~p\n",
                [ client:name( auction:winner( Auction ) ),
                  auction:max_bid( Auction ) ] ).
