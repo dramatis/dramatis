f2de79ae44751126984f68147e0bbd1496a7d88e
Steven Parkes <smparkes@smparkes.net>
2008-09-29 20:21:35 -0700

    another tweak on the observer spec/example

--- spec/dramatis/observable_spec.rb
+++ spec/dramatis/observable_spec.rb
@@ -58,4 +58,54 @@ describe "Dramatis::Actor" do
     
   end
 
+  it "should play well with observable even when we want async callbacks" do
+
+    observedClass = Class.new do
+      include Observable
+      include Dramatis::Actor
+      def foo
+        changed
+        notify_observers("foo called")
+      end
+      def add_observer actor
+        release_name = release( actor )
+        class << release_name
+          def respond_to? message
+            message == :update ? true : super
+          end
+        end
+        super release_name
+      end
+    end
+
+    observed = observedClass.new
+
+    observerClass = Class.new do
+
+      include Dramatis::Actor
+
+      attr_reader :message
+
+      def initialize observed
+        actor.always :respond_to?, true
+        observed.add_observer actor.name
+      end
+
+      def update message
+        @message = message
+      end
+
+      def doit observed
+        observed.foo
+      end
+
+    end
+
+    observer = observerClass.new observed
+    observer.message.should == nil
+    observer.doit observed
+    observer.message.should == "foo called"
+    
+  end
+
 end
