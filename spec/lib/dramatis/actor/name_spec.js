bad15bd19011ca2f8e4d53e83d85024341a0a6b5
Steven Parkes <smparkes@smparkes.net>
2010-03-31 14:29:42 -0700

    wip:internal actor name architecture

--- spec/lib/dramatis/actor/name_spec.js
+++ spec/lib/dramatis/actor/name_spec.js
@@ -4,9 +4,25 @@
     describe("actor",function(){
       describe("name",function(){
 
+        var Runtime = Dramatis.Runtime;
         var Actor = Dramatis.Actor;
         var Name = Actor.Name;
         var any = jasmine.any;
+
+        it("should allow creation of actor names from bare jids",function(){
+          expect(new Actor.Name("xmpp:user@host#actor")).toBeDefined();
+        });
+
+        it("should allow creation of actor names from full jids",function(){
+          expect(new Actor.Name("xmpp:user@host/location#actor")).toBeDefined();
+        });
+
+        it("should create remote names for remote actors",function(){
+          spyOn(Runtime.Actor.Name,"Remote");
+          expect(new Actor.Name("xmpp:user@host/location#actor")).toBeDefined();
+          expect(Runtime.Actor.Name.Remote).wasCalled();
+        });
+
 
       });
      });
